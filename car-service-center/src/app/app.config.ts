e5f9b564a1b7bb3f165c6bc8861f59f7b70aff80
Ajith-thushara <thusharat563@.com>
2024-08-08 23:12:10 +0530

    maltiple-languages

--- car-service-center/src/app/app.config.ts
+++ car-service-center/src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { routes } from './app.routes';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    ),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+  ]
 };
--- car-service-center/src/app/toolbar/toolbar.component.ts
+++ car-service-center/src/app/toolbar/toolbar.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrls: ['./toolbar.component.scss'],
+  imports: [TranslateModule] // Import TranslateModule here for standalone components
 })
 export class ToolbarComponent {
+  constructor(private translate: TranslateService) {
+    // Set default language
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
   isMenuOpen = false;
 
   toggleMenu() {
@@ -15,6 +23,14 @@ export class ToolbarComponent {
     const menu = document.getElementById('mobileMenu');
     if (menu) {
       menu.classList.toggle('hidden');
+    }
+  }
+
+  changeLanguage(event: Event) {
+    const selectElement = event.target as HTMLSelectElement | null;
+    if (selectElement) {
+      const language = selectElement.value;
+      this.translate.use(language);
     }
   }
 }
